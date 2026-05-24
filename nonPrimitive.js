2c415b7c6eaa2a839f5121251bbc2213a359b024
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-05-25 03:30:58 +0530

    new update

---
+++ nonPrimitive.js
@@ -0,0 +1,14 @@
+// non primitive data types are objects, arrays, functions, 
+//array
+
+let arr=[1,2,3]
+console.log(arr)
+console.log(typeof arr)
+
+//object
+let obj={
+    name:"GOPSL",
+    age:10
+}
+console.log(obj)
+console.log(typeof obj)
---
+++ symbol.js
@@ -0,0 +1,3 @@
+let id=Symbol('132')
+let anotherId=Symbol('132')
+console.log(id === anotherId )
