06243f762db84cc732e09a154f6f3679ff21db43
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-07-17 13:16:26 +0530

    array travaerse in js

---
+++ array.js
@@ -0,0 +1,17 @@
+// let arr=[1,8,9,4,5,6];
+// //console.log(size(arr));
+// for (i=0;i<5;i++){
+//     console.log(arr[i]);
+// }
+
+function add(a,b){
+
+    //cls
+    // console.log(
+    //let sum=a+b;
+    return a+b;
+
+}
+// console.log(
+    console.log(add(1.5,2));
+    console.log(typeof(add));
