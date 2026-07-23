4fa07403f3abfc02988145a3c829965eead117d6
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-07-23 20:21:39 +0530

    operators

---
+++ 01_operator.js
@@ -0,0 +1,44 @@
+let a=10,b=5;
+//Arithmetic operators
+// console.log("sum : ",a+b);//sum
+// console.log("subtraction",a-b);//subtraction
+// console.log("multiplication",a*b);//multiplication
+// console.log("division",a/b);//division
+//========================================
+
+//assignment Operators
+//a=20;
+// console.log(a+=1);
+// console.log(a-=1);
+// console.log(a*=1);
+// console.log(a/=1);
+//  **=
+//========================================
+ //comparison operators
+ //Equalto check ==
+
+ /*equalto and type check ===
+ not equal to check !=
+ not equal to and type check !==
+ greater than check >
+ less than check <
+ greater than or equal to check >=
+ less than or equal to check <= **/
+
+ //logical operators
+//AND operator &&
+// console.log(a>5 && b<10);//true
+// console.log(a>5 && b>10);//false
+//OR operator ||
+// console.log(a>5 || b<10);//true
+// console.log(a>5 || b>10);//true
+//NOT operator !
+// console.log(!(a>5 && b<10));//false
+// console.log(!(a>5 && b>10));//true
+
+//ternary operator
+// let age=18;
+// let result=(age>=18)?"eligible to vote":"not eligible to vote";
+// console.log(result);
+
+ 
