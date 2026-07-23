c716a4d278ef7d6fa315f0786fd151f48705dda7
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-07-23 19:30:25 +0530

    array traverse

---
+++ function.js
@@ -0,0 +1,37 @@
+"use strict";
+//console.log(a);let a=10;
+//
+
+// if(true){
+//     let a=5;
+//     var b=10;   
+// }
+// //consol
+// // (b,"b");og(a,"a");
+// console.log
+
+// let stu={
+//     a:5,
+//     b:10,
+//     name : "Gopal Da", 
+// };
+// //console.log(a,"a");
+// console.log(stu);
+// {
+//    let a=5;
+// }
+// console.log(a);
+
+const obj={
+    name: "Ball Point pen" ,
+    rating: 3 ,
+    price : 250,
+
+}
+console.log(obj);
+console.log(obj.name);
+console.log(obj["price"]);
+
+
+
+
