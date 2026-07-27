613e329822f80def86a5f377672e50156f413c28
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-07-27 13:31:37 +0530

    function update

--- function.js
+++ function.js
@@ -22,15 +22,52 @@
 // }
 // console.log(a);
 
-const obj={
-    name: "Ball Point pen" ,
-    rating: 3 ,
-    price : 250,
+// const obj={
+//     name: "Ball Point pen" ,
+//     rating: 3 ,
+//     price : 250,
 
+// }
+// console.log("\n",obj ,"\n");
+// console.log("obj.name : ",obj.name,"\n");
+// console.log('obj["price"] :',obj["price"],"\n");
+//______________________________________________________________
+
+//function without parameter and without return type
+// //function defination 
+
+// function myFunc(){
+//     console.log("Hello There...");
+// }
+
+// myFunc();//function call
+
+//______________________________________________________________
+
+//function with parameter and without return type
+// function func(str){
+//     console.log(str);
+// }
+// func("Hello There...tanmoy this side");
+
+//______________________________________________________________
+
+//function with parameter and with return type
+     // function func(str){
+     //     return str;
+     // }
+     // let a=func("Hello There...tanmoy this side");
+     // console.log(a);
+//______________________________________________________________
+
+//function with parameter and with return type
+
+function func(a,b){
+    let c=a+b;
+    return c;
 }
-console.log(obj);
-console.log(obj.name);
-console.log(obj["price"]);
+let c=func(2,2);
+console.log(c);
 
 
 
